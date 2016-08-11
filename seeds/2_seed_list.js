a80baf49ba8ea5ef689281def8b92d110173cdbd
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-08-11 12:03:10 -0600

    added in README.md'

--- seeds/2_seed_list.js
+++ seeds/2_seed_list.js
@@ -5,6 +5,8 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('list').insert({ list: 'do something fun', users_id: 1}),
+            knex('list').insert({ list: 'do something sad', users_id: 1}),
+                knex('list').insert({ list: 'do something to poop', users_id: 1}),
         knex('list').insert({ list: 'do something sad', users_id: 2}),
         knex('list').insert({ list: 'do something weird', users_id: 3})
       ]);
